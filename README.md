4c3e54d984db7e0119a4ab0cbabfeefa1d12679b
Cédric Michaux <cedric@he8us.be>
2012-09-25 02:31:36 +0200

    Cleaned for submodules

--- README.md
+++ README.md
@@ -3,8 +3,3 @@ puppet-manifests
 
 My puppet modules for Vagrant
 
-
-TODO
-----
-
-1. Mysql dump
--- mysql/manifests/create.pp
+++
@@ -1,8 +0,0 @@
-class mysql::create($dbuser = "user", $dbpass = "secret", $dbname = 'puppet'){
-
-	exec { 'create-db':
-		unless => "/usr/bin/mysql -u${dbuser} -p${dbpass} ${dbname}",
-		command => "/usr/bin/mysql -e \"create database ${dbname}; grant all on ${dbname}.* to ${dbuser}@localhost identified by '${dbpass}';\"",
-		require => Service["mysql"],
-	}
-}
